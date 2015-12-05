87a5da890b3fbd457989113fd4451be9e67991df
Xinyu Zhao <xinyu.zhao@gao.io>
2015-12-04 23:36:03 -0600

    process input and output in the same module

--- NewUserBooking.py
+++ NewUserBooking.py
@@ -12,6 +12,23 @@ def loadInput (fileName):
     data = pd.read_csv(fileName, header = 0)
     return data
 
+def writeOutput (fileName, data):
+    data.to_csv(fileName, sep = ',', index = False) 
+    return
+
+def formatOutput (InputFileName, OutputFileName, encoderFile):
+    testData = loadInput(InputFileName)
+    output = loadInput(OutputFileName)
+    encoder = pd.read_csv(encoderFile, header = None)
+    
+    label = output['label']
+    testId = testData['id']
+    testcountry = label.apply(lambda x: encoder[0][x])
+
+    result = pd.DataFrame({'id': testId, 'country': testcountry})
+    result.to_csv('submission.csv', header=True, index=False, columns=['id', 'country'])
+    return
+
 def cleanData (data):
     del data['id']
     ################### convert date to numeric ##############
@@ -37,16 +54,15 @@ def cleanData (data):
     
     data.fillna('empty', inplace = True)
     ############ encoding categorical features ####################
-    le = preprocessing.LabelEncoder()
     for col in featureNames:
         if data[col].dtype != np.float64 and data[col].dtype != np.int64: 
             print(col)
+            le = preprocessing.LabelEncoder()
             data[col] = le.fit_transform(data[col])
+            if col == 'country_destination':
+                np.savetxt("country_destination_encoder_list.csv", 
+                        le.classes_, delimiter=",", fmt = '%s')
     return data  
-
-def writeOutput (fileName, data):
-    data.to_csv(fileName, sep = ',') 
-    return
 
 def knnclassify (X, Y):
     accuracy = []
@@ -64,19 +80,18 @@ def knnclassify (X, Y):
     return clf
 
 def main():
-#    trainData = loadInput('test_users.csv')
-#    trainData = cleanData(trainData)
-#    writeOutput('cleaned_test_data.csv', trainData)
+    trainData = loadInput('train_users.csv')
+    trainData = cleanData(trainData)
+    writeOutput('cleaned_train_data.csv', trainData)
+    testData = loadInput('test_users.csv')
+    testData = cleanData(testData)
+    writeOutput('cleaned_test_data.csv', trainData)
     trainData = loadInput('cleaned_train_data.csv')
     testData = loadInput('cleaned_test_data.csv')
-
     ########## remove features with zero variance #################
     del trainData['date_account_created_yy']
     del testData['date_account_created_yy']
     ############### convert dataframe to ndarray ##################
-    del trainData['Unnamed: 0']
-    del testData['Unnamed: 0']
-
     trainY = trainData['country_destination'].values
     del trainData['country_destination']
     trainData = trainData.values
@@ -89,6 +104,9 @@ def main():
     predictLabel = clf.predict(testData)
     result = pd.DataFrame({'label':predictLabel})
     writeOutput('test_data_label.csv', result)
+
+    formatOutput('test_users.csv', 'test_data_label.csv', 
+            'country_destination_encoder_list.csv')
 
     data['train'] = trainData
     data['trainY'] = trainY
